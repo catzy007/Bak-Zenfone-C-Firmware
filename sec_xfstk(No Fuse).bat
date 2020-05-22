bedd4dfd7fac8cab7d09900d8ea3d85deba049dd
catzy007 <catzy.rais@gmail.com>
2020-05-23 00:58:56 +0700

    initial commit

---
+++ sec_xfstk(No Fuse).bat
@@ -0,0 +1,2 @@
+@echo off
+xfstk-dldr-solo.exe -v --fwdnx sec-dnx_fwr.bin --fwimage sec-ifwi-prod.bin --gpflags 0x80000305 --softfuse soft_fuse.bin --osdnx sec-dnx_osr.bin --osimage droidboot.img.POS_sign.bin
